30d07869d0e1aeb1200ced2b1554221695cfec89
fiorella <fiorellachilc_20@hotmail.com>
2020-05-15 13:59:55 -0500

    CRUD

--- src/main.js
+++ src/main.js
@@ -4,22 +4,85 @@ const init = () => {
     window.addEventListener('hashchange', () => {
         changeView(window.location.hash)
     });
-    const notesList = document.getElementById('notes-list');
-    console.log(notesList);
 };
 
 window.addEventListener('load', init);
+const renderNote = (dataAndId) => {
+    const newData = dataAndId[0].map(doc => {
+        const note = doc.data();
+        const li = document.createElement('li');
+        const divNote = document.createElement('div');
+        const divOptions = document.createElement('div');
+        const divNoteTittle = document.createElement('div');
+        const divNoteContent = document.createElement('div');
+        const removeButton = document.createElement('button');
+        const editButton = document.createElement('button');
+        const saveButton = document.createElement('i');
+        saveButton.className = 'fas fa-save';
+        divNote.className = 'li-note';
+        divNoteTittle.textContent = note.tittle;
+        divNoteContent.textContent = note.content;
+        divOptions.className = 'li-options';
+        divOptions.setAttribute('data-id', doc.id);
+        removeButton.className = 'remove';
+        editButton.className = 'edit';
+        removeButton.innerHTML = '<i class="fas fa-times"></i>';
+        editButton.innerHTML = '<i class="fas fa-pen"></i>';
+        divNote.appendChild(divNoteTittle);
+        divNote.appendChild(divNoteContent);
+        divOptions.appendChild(removeButton);
+        divOptions.appendChild(editButton);
+        li.appendChild(divNote);
+        li.appendChild(divOptions);
+        // EDIT A DOC
+        editButton.addEventListener('click', () => {
+            divNoteContent.contentEditable = 'true';
+            divNoteContent.focus();
+            divNote.appendChild(saveButton);
+        });
+        saveButton.addEventListener('click', () => {
+            divNoteContent.contentEditable = 'false';
+            divNote.removeChild(saveButton);
+            db.collection(dataAndId[1]).doc(doc.id).update({
+                content: divNoteContent.textContent
+            })
+        });
+         // DELETE A DOC
+             removeButton.addEventListener('click', () => {
+                 db.collection(dataAndId[1]).doc(doc.id).delete();
+             });
+        return li;
+    });
+    return newData;
+};
+
 auth.onAuthStateChanged((user) => {
     if (user) {
         window.location.hash = '#/appNote';
         changeView(window.location.hash);
-        //FIRESTORE GET DATA 
-        // db.collection(user.uid).onSnapshot((snapshot) => {
-        //     // passing an array of documents
-        //     setUpNotes(snapshot.docs);
-        // }, (err) => {
-        //     console.log(err.message);
-        // });
+        const notesList = document.getElementById('notes-list');
+        // FIRESTORE GET DATA 
+        db.collection(user.uid).onSnapshot((snapshot) => {
+            notesList.innerHTML = '';
+            // passing an array of documents
+            renderNote([snapshot.docs, user.uid]).forEach((li) => {
+                notesList.appendChild(li);
+            })
+        });
+        // CREATE NOTES
+        const createForm = document.getElementById('notes-form');
+        createForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            db.collection(user.uid).add({
+                // it can be also called with dot notation createForm.tittle
+                tittle: createForm['tittle'].value,
+                content: createForm['content'].value
+            }).then(() => {
+                createForm.reset();
+            }).catch((err) => {
+                console.log(err.message);
+            })
+        });
     } else {
         window.location.hash = '#/';
         changeView(window.location.hash);
--- src/views/app-note.js
+++ src/views/app-note.js
@@ -22,50 +22,6 @@ export default () => {
             </div>
         </main>`;
     div.innerHTML = appNoteView;
-    div.id = 'divNotes';
-    // SET UP NOTES
-    const noteList = div.querySelector('#notes-list');
-    const setUpNotes = (data) => {
-        if (data.length) {
-            let html = '';
-            data.forEach(doc => {
-                const note = doc.data();
-                const li = `
-              <li>
-                  <div class="collapsible-header">${note.tittle}</div>
-                  <div class="collapsible-body">${note.content}</div>
-              </li>`;
-                html += li;
-            });
-            noteList.innerHTML = html;
-        }
-    };
-    auth.onAuthStateChanged((user) => {
-        if (user) {
-            //FIRESTORE GET DATA 
-            db.collection(user.uid).onSnapshot((snapshot) => {
-                // passing an array of documents
-                setUpNotes(snapshot.docs);
-            }, (err) => {
-                console.log(err.message);
-            });
-        }
-    });
-    // CREATE GUIDE
-const createForm = div.querySelector('#notes-form');
-createForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    db.collection('yEhcndg2MYXvJzeCZqoaTTVEm5Z2').add({
-        // it can be also clled with dot notation createForm.tittle
-        tittle: createForm['tittle'].value,
-        content: createForm['content'].value
-    }).then(() => {
-        createForm.reset();
-    })
-    //.catch((err) => {
-    //     console.log(err.message);
-    // })
-});
     // LOG OUT
     const logout = div.querySelector('#logout');
     logout.addEventListener('click', () => {
